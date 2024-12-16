51f76a5dc4c0529dbd08867ed3b43630d87ba52a
s.bagherzadeh <s.bagherzadeh@PC_634>
2024-12-16 11:45:04 +0330

    Add project files.

---
+++ exercise 4/Program.cs
@@ -0,0 +1,21 @@
+﻿string Number1 = Console.ReadLine();
+double a = Convert.ToDouble(Number1);
+string Number2 = Console.ReadLine();
+double b = Convert.ToDouble(Number2);
+string Number3 = Console.ReadLine();
+double c = Convert.ToDouble(Number3);
+double determine = b * b - 4 * a * c;
+if (determine >0) 
+{
+    double X1 = (-b + Math.Sqrt(determine)) / (2 * a);
+    double X2 = (-b - Math.Sqrt(determine)) / (2 * a);
+    Console.WriteLine($" javabe aval shoma: {X1} , javabe dovome shoma: {X2}");
+}
+else if (determine == 0) { 
+    double X = -b / (2 * a);
+    Console.WriteLine($"javabe X shoma barabare: {X}");
+}
+else
+{
+    Console.WriteLine("rahe hali vojod nadarad");
+}
